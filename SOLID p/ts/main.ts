f119ea4e6fafef61495063af4c4a39cb1a30f585
Veerpal Kaur <veerpal.kaur@sourcefuse.com>
2025-06-13 17:09:56 +0530

    solid

---
+++ SOLID p/ts/main.ts
@@ -0,0 +1,18 @@
+import { BookManager, RenderBooks, HandleSubmit } from './bookManager.js';
+
+// create instance of bookManager
+const manager = new BookManager();
+
+// create instance of RenderBooks
+const renderer = new RenderBooks();
+renderer.books = manager.books;  // pass the manager books into render books
+renderer.manager = manager;  // link manager to renderer
+renderer.renderBooks();   // callingt the func
+
+
+
+const handleSubmit = new HandleSubmit();
+handleSubmit.books = manager.books;
+handleSubmit.manager = manager; // link manager to handler
+handleSubmit.renderer=renderer;
+
